4e48c65571672856491948dadddbf5b35d2f4774
RinHizakura <s921975628@gmail.com>
2023-01-31 23:59:34 +0800

    263-ugly-number

---
+++ 263-ugly-number/src/main.rs
@@ -0,0 +1,20 @@
+struct Solution;
+impl Solution {
+    pub fn is_ugly(n: i32) -> bool {
+        if n == 0 {
+            return false;
+        }
+        let mut n = n;
+        for factor in [2, 3, 5] {
+            while n % factor == 0 {
+                n /= factor;
+            }
+        }
+
+        n == 1
+    }
+}
+
+fn main() {
+    println!("{}", Solution::is_ugly(5));
+}
