4a895572f7b4742bef1d3ef16ea31747cda0297d
RinHizakura <s921975628@gmail.com>
2023-04-28 00:37:48 +0800

    Optimize the solution of 406

--- 406-queue-reconstruction-by-height/src/main.rs
+++ 406-queue-reconstruction-by-height/src/main.rs
@@ -10,30 +10,14 @@ impl Solution {
          * sort works because of the assumption that the answer should always
          * exist */
         let len = people.len();
+        let mut result = Vec::with_capacity(len);
+        result.push(people[0].clone());
+
         for i in 1..len {
-            let h = people[i][0];
-            let f = people[i][1];
-            let mut front = people[i][1];
-
-            let mut j = 0;
-            while front > 0 {
-                if people[j][0] >= h {
-                    front -= 1;
-                }
-                j += 1;
-            }
-            /* Until here, j is the place we should insert */
-
-            for idx in (j..i).rev() {
-                people[idx + 1][0] = people[idx][0];
-                people[idx + 1][1] = people[idx][1];
-            }
-
-            people[j][0] = h;
-            people[j][1] = f;
+            result.insert(people[i][1] as usize, people[i].clone());
         }
 
-        people
+        result
     }
 }
 
